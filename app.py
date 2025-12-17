aecc2d2d9eb16fe6ae8e70d06d084064e777ca42
鸡哥上的twinkll <976019520@qq.com>
2025-12-17 17:26:57 +0800

    test by Cheng

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Docker CI/CD World! v2.0'
+    return 'Hello, Docker CI/CD World! v2.0 test by Cheng'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
